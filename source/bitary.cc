4bde546222c430fddc3efba06d71a04ad0c8adc6
xfleury <xfleury@34b7af81-6bd9-470d-b59e-02e4460bf6c7>
2013-05-14 22:04:54 +0000

    - Added copy constructor to bit_vector class to fix delete[] called.  - Fixed a crash in directn.cc that would occur if a creature had empty mname.

--- source/bitary.cc
+++ source/bitary.cc
@@ -9,6 +9,15 @@
 
 #include "debug.h"
 
+bit_vector::bit_vector(const bit_vector& other) : size(other.size)
+{	
+    nwords = static_cast<int>((size + LONGSIZE - 1) / LONGSIZE);
+    data = new unsigned long[nwords];
+    for (int w = 0; w < nwords; ++w)
+        data[w] = other.data[w];
+}
+
+
 bit_vector::bit_vector(unsigned long s)
     : size(s)
 {
